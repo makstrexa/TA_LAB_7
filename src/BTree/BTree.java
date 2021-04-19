9b855149dfc85682152f280855b95a5f4ac53f43
makstrexa <73738747+makstrexa@users.noreply.github.com>
2021-04-19 09:14:15 +0300

    Update BTree.java

--- src/BTree/BTree.java
+++ src/BTree/BTree.java
@@ -5,7 +5,6 @@ class BTree{
     BTreeNode root;
     int MinDeg;
 
-    // Constructor
     public BTree(int deg){
         this.root = null;
         this.MinDeg = deg;
@@ -30,14 +29,10 @@ class BTree{
             root.num = 1;
         }
         else {
-            // When the root node is full, the tree will grow high
             if (root.num == 2*MinDeg-1){
                 BTreeNode s = new BTreeNode(MinDeg,false);
-                // The old root node becomes a child of the new root node
                 s.children[0] = root;
-                // Separate the old root node and give a key to the new node
                 s.splitChild(0,root);
-                // The new root node has 2 child nodes. Move the old one over there
                 int i = 0;
                 if (s.keys[0]< key)
                     i++;
@@ -58,9 +53,7 @@ class BTree{
 
         root.remove(key);
 
-        if (root.num == 0){ // If the root node has 0 keys
-                            // If it has a child, its first child is taken as the new root,
-                            // Otherwise, set the root node to null
+        if (root.num == 0){ 
             if (root.isLeaf)
                 root = null;
             else
